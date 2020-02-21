db834d5bd1f779f9a0dee59e9febd501a843cd75
Atul R <atulanand94@gmail.com>
2020-02-21 20:52:22 +0100

    add basic tar compress

---
+++ README.md
@@ -0,0 +1,22 @@
+# Tar Github Action
+
+A cross platform github action for using tar.
+Works on Windows, Mac and Linux vms.
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+uses: actions/hello-world-javascript-action@v1
+with:
+who-to-greet: 'Mona the Octocat'
---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require("@actions/core");
+const tar = require("tar");
+
+try {
+  const cwd = core.getInput("cwd");
+  const command = core.getInput("command", { required: true });
+  const files = core.getInput("files", { required: true });
+  const archiveName = core.getInput("archiveName", { require: true });
+
+  switch (command) {
+    case "c": {
+      tar.c({ cwd, name: archiveName, sync: true }, files);
+      break;
+    }
+    default:
+      throw new Error(`Unsupported command: ${command}`);
+  }
+
+  core.setOutput("done", true);
+} catch (error) {
+  core.setFailed(error.message);
+}
