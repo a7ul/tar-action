f61b2970a19ec427adb4775b063483901651e618
Michael Möhler <m.moehler@xf9.de>
2022-03-20 22:20:23 +0100

    Add extract command

--- src/index.js
+++ src/index.js
@@ -9,15 +9,24 @@ try {
     .getInput("files", { required: true })
     .split("\n")
     .filter(x => x !== "");
-  const outPath = core.getInput("outPath", { require: true });
+  const outPath = core.getInput("outPath");
 
   const listOfFiles = Array.isArray(files) ? files : [files];
 
   switch (command) {
     case "c": {
+      if(!outPath)
+        throw new Error("Please specify an out path");
       tar
         .c({ cwd, gzip: true, sync: true }, listOfFiles)
         .pipe(fs.createWriteStream(outPath));
+      break;
+    }
+    case "x": {
+      if(files.length !== 1)
+        throw new Error("Only one file can be extracted");
+      tar
+        .x({ C: cwd, sync: true, file: files[0] });
       break;
     }
     default:
